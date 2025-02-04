fe8e3764fadbf4caba03f0e58bfb3c856f623949
MarkIvanchenko <ivanchenko.mark@inbox.ru>
2025-02-04 23:46:07 +0300

    закоментировал строки 6 и 7 VetClinicApp

--- src/main/java/ru/aston/Ivanchenko_MT/Clinik/VetClinicApp.java
+++ src/main/java/ru/aston/Ivanchenko_MT/Clinik/VetClinicApp.java
@@ -3,8 +3,8 @@ package ru.aston.Ivanchenko_MT.Clinik;
 public class VetClinicApp {
     public static void main(String[] args) {
         // Инициализация объекта для работы с заказами
-        User user1 = new User("Иван", "Иванов", 30, "male");
-        User user2 = new User("Анна", "Петрова", 65, "female");
+        // User user1 = new User("Иван", "Иванов", 30, "male");
+        // User user2 = new User("Анна", "Сергеевна", 65, "female");
 
         // Создайте несколько заказов
         Order consultation = new Consultation(1, user1);
